80ff37fb7ee2af317df5e84eaadc58bb000c1d4f
per <per@naavik.no>
2024-01-28 21:45:21 +0100

    bytter getopt med typer

--- losningsforslag/07-argsparse_typer.py
+++ losningsforslag/07-argsparse_typer.py
@@ -1,11 +1,13 @@
 import argparse
+import typer
+from typing import Optional
 
 def streng_func(s):
     print("Lengden på strengen er ", len(s))
     for i in range(1,len(s),2):
         print("Ny streng ", s[i])
 
-def main():
+def argsparse_example():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', type=int,required=False, help='Input ett tall')
     parser.add_argument('-s', type=str,required=False, help='Input en streng')
@@ -14,5 +16,16 @@ def main():
          print([*range(0,int(args.c)+1)])  
     elif args.s:
         streng_func(args.s)
+
+def typer_example(
+          tall: Optional[int] = typer.Option(default=None, help="Input ett tall"),
+          streng: Optional[str] = typer.Option(default=None,help="Input en streng"),
+    ):
+    if tall is not None:
+        print([*range(0,int(tall)+1)]) 
+    if streng is not None:
+        streng_func(streng)
+
 if __name__ == '__main__':
-    main()
+    #argsparse_example()
+    typer.run(typer_example)
